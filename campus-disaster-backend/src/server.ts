a7720771f506dc422afcd41705811bc32e916afd
NamanJain45 <namanjain1593@gmail.com>
2026-08-24 12:45:10 +0530

    fix: bind backend on configured host and handle shutdown

--- campus-disaster-backend/src/server.ts
+++ campus-disaster-backend/src/server.ts
@@ -1,15 +1,20 @@
 import app from "./app";
+import { env } from "./config/env";
 
-import {
-  env
-} from "./config/env";
+const server = app.listen(env.port, env.host, () => {
+  console.log(
+    `Campus Disaster backend running at http://${env.host}:${env.port}`
+  );
+});
 
-app.listen(
-  env.port,
-  () => {
+const shutdown = (signal: string) => {
+  console.log(`${signal} received. Shutting down backend...`);
 
-    console.log(
-      `Campus Disaster backend running on port ${env.port}`
-    );
-  }
-);
+  server.close(() => {
+    console.log("Campus Disaster backend stopped.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGINT", () => shutdown("SIGINT"));
